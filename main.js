003f281e2ddfb3c22df0ddc2917976cb0aff0b21
NikolozLekishvili <K0K0L0z1922@gmail.com>
2026-02-04 07:47:35 +0400

    1

---
+++ main.js
@@ -0,0 +1,75 @@
+// დავალება 1
+const input = document.querySelector("#num");
+const msg = document.querySelector("#msg");
+
+input.addEventListener("keyup", (e) => {
+  if (!/[0-9]/.test(e.key) && e.key.length === 1) {
+    msg.textContent = "მხოლოდ ციფრები შეიყვანე!";
+  } else {
+    msg.textContent = "";
+  }
+});
+
+// დავალება 2
+
+  const input1 = document.querySelector("#textInput");
+  const msg1 = document.querySelector("#msg1");
+
+  input1.addEventListener("keyup", () => {
+    const words = "javascript";
+    if (input1.value === words) {
+      msg1.textContent = "სწორია";
+    } else {
+      msg1.textContent = "შეიყვანე javascript";
+    }
+  });
+
+//დავალება 3
+  const fileInput = document.querySelector("#fileInput");
+    const preview = document.querySelector("#preview");
+
+    fileInput.addEventListener("change", () => {
+      const file = fileInput.files[0]; 
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          preview.src = reader.result; 
+        };
+        reader.readAsDataURL(file); 
+      }
+    });
+
+//დავალება 4
+  const fileInput1 = document.querySelector("#fileInput");
+    const info = document.querySelector("#info");
+
+    fileInput1.addEventListener("change", () => {
+      const file = fileInput1.files[0]; 
+      if (file) {
+        const sizeKB = Math.round(file.size / 1024);
+        const type = file.type;
+
+        info.textContent = `სურათის ზომა: ${sizeKB}KB | ტიპი: ${type}`;
+      }
+    });
+
+//დავალება 5
+//   const fileInput2 = document.querySelector("#fileInput");
+//     const base64Area = document.querySelector("#base64");
+
+//     fileInput2.addEventListener("change", () => {
+//       const file = fileInput.files[0];
+//       if (file) {
+//         const reader = new FileReader();
+//         reader.onload = () => {
+         
+//           base64Area.textContent = reader.result;
+//         };
+//         reader.readAsDataURL(file);
+//       }
+//     });
+
+
+
+
+
